d47f275faf23e766d21a655c583857fc7ffc54e2
Sandeep Raveesh <sanravee@amazon.com>
2026-02-20 12:53:55 -0800

    Feature SageMaker Endpoint LLM Inference Monitoring with MLflow

--- monitoring/sagemaker-endpoint-llm-monitoring/cdk/lib/sagemaker-inference-monitoring-stack.d.ts
+++ monitoring/sagemaker-endpoint-llm-monitoring/cdk/lib/sagemaker-inference-monitoring-stack.d.ts
@@ -7,7 +7,7 @@ export interface SageMakerInferenceMonitoringStackProps extends cdk.StackProps {
      * Existing S3 bucket name where SageMaker data capture is stored
      * If not provided, will look for default SageMaker bucket
      */
-    dataCaptureS3BucketName?: string;
+    dataCaptureS3BucketName: string;
     /**
      * cdk stack prefix
      * @default ''
